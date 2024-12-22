7471ef8416268b65a27ebd89951e093662817939
Oluwatobi Oloro <101267703+Tobiwealth@users.noreply.github.com>
2024-12-22 14:21:09 +0100

    newww

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+import { Home } from './components/Home'
+import { Login } from './components/Login'
+
+function App() {
+  const [username, setUsername] = useState('')
+
+  return username
+    ? <Home username={username} />
+    : <Login onSubmit={setUsername} />
+}
+
+export default App
